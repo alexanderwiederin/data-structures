48b92947899d5368573673b5770333b8578c7ffb
alexanderwiederin <alexander.wiederin@gmail.com>
2018-09-21 16:24:49 -0700

    Attempted Advanced Hash Table.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,6 +4,7 @@ var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
   this.getIndexBelowMaxForKey = getIndexBelowMaxForKey;
+  this.count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -12,6 +13,8 @@ HashTable.prototype.insert = function(k, v) {
   value[0] = k;
   value[1] = v;
   this._storage.set(index,value);
+  this.count++;
+  // create a helper funciton
   // node inserted into storage at index
 };
 
@@ -21,12 +24,32 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+  if(count === 0) {
+    return 'Error: nothing to remove.';
+  }
   var index = this.getIndexBelowMaxForKey(k, this._limit);
   delete this._storage.get(index)[0];
   delete this._storage.get(index)[1];
-
+  this.count--;
+  // if this._storage
 };
+/*
 
+helper function called doubleLimitedArray that 
+  1. creates a new limitedArray with limit equal to 2x the old limitedArray
+  2. iterates over the old limitedArray
+    a. invokes insert(limitedArray[i]) which: 
+      i. defines a new index for the item at the old index
+      ii. places in the new limitedArray
+      
+helper function called halveLimitedArray that
+  1. creates a new limitedArray with limit equal to 0.5x the old limitedArray
+  2. iterates over the old limitedArray
+    a. invokes insert(limitedArray[i]) which: 
+      i. defines a new index for the item at the old index
+      ii. places in the new limitedArray
+   
+*/
 
 
 /*
